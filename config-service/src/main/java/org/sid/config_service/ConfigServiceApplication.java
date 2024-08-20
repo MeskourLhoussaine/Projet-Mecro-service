f22b04e34226a43045f479da8001e8d2d6763e3e
MeskourLhoussaine <127257257+MeskourLhoussaine@users.noreply.github.com>
2024-08-20 20:04:05 +0100

    commit

--- config-service/src/main/java/org/sid/config_service/ConfigServiceApplication.java
+++ config-service/src/main/java/org/sid/config_service/ConfigServiceApplication.java
@@ -4,10 +4,12 @@ package org.sid.config_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
--- customer-service/src/main/java/org/sid/customer_service/CustomerServiceApplication.java
+++ customer-service/src/main/java/org/sid/customer_service/CustomerServiceApplication.java
@@ -1,13 +1,32 @@
 package org.sid.customer_service;
 
+import org.sid.customer_service.entities.Customer;
+import org.sid.customer_service.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @SpringBootApplication
+
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
-
+@Bean
+CommandLineRunner start(CustomerRepository repository) {
+		return args -> {
+			repository.saveAll(List.of(
+					Customer.builder().firstName("meskour").email("mesk@gmail.com").build(),
+					Customer.builder().firstName("lhoussaine").email("louis@gmail.com").build(),
+					Customer.builder().firstName("anas").email("anas@gmail.com").build()
+			));
+			repository.findAll().forEach(System.out::println);
+		};
+}
 }
